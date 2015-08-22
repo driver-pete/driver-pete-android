6aefee26ab313c13a6d7bb6095a66fb03c7ddaaf
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-08-22 09:57:50 -0700

    new api

--- app/src/main/java/com/otognan/driverpete/android/DriverPeteServer.java
+++ app/src/main/java/com/otognan/driverpete/android/DriverPeteServer.java
@@ -1,8 +1,11 @@
 package com.otognan.driverpete.android;
 
+import java.util.List;
+
 import retrofit.Callback;
 import retrofit.client.Response;
 import retrofit.http.Body;
+import retrofit.http.DELETE;
 import retrofit.http.GET;
 import retrofit.http.POST;
 import retrofit.http.Query;
@@ -15,4 +18,22 @@ public interface DriverPeteServer {
 
     @POST("/api/trajectory/compressed")
     void uploadCompressedTrajectory(@Query("label") String label, @Body TypedInput body, Callback<Response> callback);
+
+    @GET("/api/trajectory/endpoints")
+    public void trajectoryEndpoints(Callback<List<TrajectoryEndpoint>> callback);
+
+    @GET("/api/trajectory/routes")
+    public void routes(@Query("isAtoB") boolean isAtoB, Callback<List<String>> callback);
+
+    @DELETE("/api/trajectory/state")
+    public void resetProcessorState(Callback<Response> callback);
+
+    @DELETE("/api/trajectory/endpoints/all")
+    public void deleteAllEndpoints(Callback<Response> callback);
+
+    @DELETE("/api/trajectory/routes/all")
+    public void deleteAllRoutes(Callback<Response> callback);
+
+    @POST("/api/trajectory/reprocess/all")
+    public void reprocessAllUserData(Callback<Response> callback);
 }
