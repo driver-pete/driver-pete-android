61ed606e64779913898fb550172a656fedb2bd71
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-08-22 09:59:01 -0700

    endpoint class

---
+++ app/src/main/java/com/otognan/driverpete/android/TrajectoryEndpoint.java
@@ -0,0 +1,75 @@
+package com.otognan.driverpete.android;
+
+
+public class TrajectoryEndpoint {
+    private Long id;
+
+    private double latitude;
+
+    private double longitude;
+
+    private int number;
+
+    private String label;
+
+    private String address;
+
+    private User user;
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
+
